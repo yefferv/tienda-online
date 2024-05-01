c5b07ae3489a3562a6b82b050a90cd6877564908
mamartinezb <mamartinezb@indracompany.com>
2024-04-30 21:34:03 -0500

    Se adiciona el hook useApi, se cuadra el loadiing y se empieza a manejar la variable de entorno (archivo .env)

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,42 +1,24 @@
 import CrCard from "../components/CrCard"
-import CrNavbar from "../components/CrNavbar"
-import { useEffect, useState } from "react";
-import { Product } from "../types/Product";
 import { Box, Container, Grid } from "@mui/material";
 import HomeLayaout from "./HomeLayaout";
+import useApi from "./hook/useApi";
+import CenteredCircularProgress from "../components/CenteredCircularProgress";
 
 
 
 const Home = () => {
-
-  
-  const [data, setdata] = useState<Product[]>([]);
-  const [loadin, setLoadin] = useState(false);
-
-  useEffect(() => {
-    setLoadin(true)
-    fetch('https://fakestoreapi.com/products')
-        .then(res=>res.json())
-        .then(json=>setdata(json))
-        .catch(err=>console.log(err))
-        .finally(()=>{
-          setLoadin(false)
-          console.log('fetch finalizado')
-
-        })
-  }, []);
+  const {data, loading} = useApi(import.meta.env.VITE_API_URL)
 
 
   return (
     <HomeLayaout>
       <Container maxWidth="lg">
-        <Box sx={{display:'flex',
-            marginTop: 5,
-            gap: 2
-        }}>
-            {loadin?<p>Loading...</p>:
+        <Box mt={5} display={'flex'} gap={2} >
+            {loading?
+            <CenteredCircularProgress />
+            :
             <Grid container spacing={2}>
-            {data.map((item)=>
+            {data.map((item : any)=>
                 <Grid item key={item.id} xs={12} sm={6} md={4} lg={3} xl={6}>
                   <CrCard item = {item}/>
                 </Grid>
---
+++ src/services/useServices.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+import { Product } from "../types/Product"
+
+const useServices = (url: string) => {
+
+    const [data, setData] = useState<Product[]>([])
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<boolean>(false)
+
+  
+
+    const handle = async () => {
+      setLoading(true)
+      try {
+        const data = await fetch(url)
+        const response = await data.json()
+        setData(response)
+      }catch(error){
+        setError(true)
+      }finally{
+        setLoading(false)
+      }
+      
+    }
+
+  return {
+    data,
+    handle,
+    loading
+  }
+}
+
+export default useServices
